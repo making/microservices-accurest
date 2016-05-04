4a748ed10515ff8fc9a6f9bf471771b0c13d263b
making <makingx@gmail.com>
2016-05-04 12:48:20 +0900

    Polish Contract

--- microservices-accurest-provider/src/test/resources/accurest/requestForFoos.groovy
+++ microservices-accurest-provider/src/test/resources/accurest/requestForFoos.groovy
@@ -1,15 +1,11 @@
 io.codearte.accurest.dsl.GroovyDsl.make {
     request {
         method 'GET'
-        url '/foos'
+        urlPath '/foos'
     }
     response {
         status 200
-        body( """[ {
-        "value" : 42
-      }, {
-        "value" : 100
-      } ]""")
+        body([[value: 42], [value: 100]])
         headers {
             header('Content-Type': 'application/json;charset=UTF-8')
         }
